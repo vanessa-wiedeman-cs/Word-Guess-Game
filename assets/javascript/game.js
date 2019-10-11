bcb65f6a533a7a0b2cfa856977ced8d5ff0bcd56
Vanessa <vanessa.wiedeman.cs@gmail.com>
2019-10-10 20:33:59 -0500

    Working word guess game as Obj

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,55 @@
 
+const game = {
+	
+	answer 		: "",
+	display 	: "",
+	index 		: 0,
+	showLetter 	:[],
+
+	startGame: function() {
+		
+		wordBank = ["halloween" , "spooky", "ghosts"];
+		document.getElementById("word").innerHTML = "";
+		index = Math.floor(Math.random() * wordBank.length);
+		answer = wordBank[index];
+		this.answer = answer;
+		for( x = 0; x < answer.length; x++) {
+			document.getElementById("word").innerHTML += "_";
+			this.showLetter.push(0);
+		}
+
+	},
+
+	guess: function() {
+		text = document.getElementById("letter");
+		guess = "";
+		guess = text.value;
+		guess = guess.toLowerCase();
+		//document.getElementById("testDisplay").innerHTML = guess;
+		document.getElementById("word").innerHTML = "";
+
+		num = this.answer.length;
+		
+		//document.getElementById("testDisplay").innerHTML = num;
+
+		for(x = 0; x < num; x++) {
+			document.getElementById("testDisplay").innerHTML = x + "<br>"
+			if(guess == this.answer[x]) {
+				this.showLetter[x] = 1;
+			}	
+		}
+
+		for(y = 0; y < this.answer.length; y++) {
+			if(this.showLetter[y] == 0) {
+				document.getElementById("word").innerHTML += "_";
+			}
+			if(this.showLetter[y] == 1) {
+				document.getElementById("word").innerHTML += this.answer[y];
+			}
+		}
+	} 
+
+}
+	
 
 
