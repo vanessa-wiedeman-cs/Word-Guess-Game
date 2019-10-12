2d813437d587468c8083b71ef5393549f006ea2a
Vanessa <vanessa.wiedeman.cs@gmail.com>
2019-10-12 10:41:42 -0500

    Made game div a bootstrap card for easier aligning

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,7 @@ const game = {
 	display 	: "",
 	index 		: 0,
 	points		: 0,
+	guessLeft	: 10,
 	showLetter 	:[],
 
 	startGame: function() {
@@ -26,36 +27,46 @@ const game = {
 		guess = "";
 		guess = text.value;
 		guess = guess.toLowerCase();
+
 		win = 0;
-		//document.getElementById("testDisplay").innerHTML = guess;
-		document.getElementById("word").innerHTML = "";
-		document.getElementById("lastGuess").innerHTML += guess + " ";
 
-		num = this.answer.length;
-		
-		//document.getElementById("testDisplay").innerHTML = num;
-
-		for(x = 0; x < num; x++) {
-			document.getElementById("testDisplay").innerHTML = x + "<br>";
-			if(guess == this.answer[x]) {
-				this.showLetter[x] = 1;
-			}	
-		}
-
-		for(y = 0; y < this.answer.length; y++) {
-			if(this.showLetter[y] == 0) {
-				document.getElementById("word").innerHTML += "_";
-			}
-			if(this.showLetter[y] == 1) {
-				document.getElementById("word").innerHTML += this.answer[y];
-				win++;
-			}
-		}
-
-		if(win == num)
+		if(guess.length == 1)
 		{
-			this.win();
-			document.getElementById("lastGuess").innerHTML = "Last Guess: "
+			this.guessLeft = this.guessLeft - 1;
+
+			//document.getElementById("testDisplay").innerHTML = guess;
+			document.getElementById("word").innerHTML = "";
+			document.getElementById("lastGuess").innerHTML += guess + " ";
+
+			num = this.answer.length;
+			
+			//document.getElementById("testDisplay").innerHTML = num;
+
+			for(x = 0; x < num; x++) {
+				//document.getElementById("testDisplay").innerHTML = x + "<br>";
+				if(guess == this.answer[x]) {
+					this.showLetter[x] = 1;
+				}	
+			}
+
+			for(y = 0; y < this.answer.length; y++) {
+				if(this.showLetter[y] == 0) {
+					document.getElementById("word").innerHTML += "_";
+				}
+				if(this.showLetter[y] == 1) {
+					document.getElementById("word").innerHTML += this.answer[y];
+					win++;
+				}
+			}
+
+			if(win == num)
+			{
+				this.win();
+			}
+			if(this.guessLeft == 0)
+			{
+				this.lose();
+			}
 		}
 
 	},
@@ -63,6 +74,20 @@ const game = {
 	win(){
 
 		this.points++;
+		this.guessLeft = 10;
+		document.getElementById("testDisplay").innerHTML = "Points: " + this.points;
+		document.getElementById("lastGuess").innerHTML = "Last Guess: ";
+		for(x = 0; x < this.answer.length; x++){
+			this.showLetter.pop(x);			
+		}
+		this.startGame();
+	},
+
+	lose()
+	{
+		this.guessLeft = 10;
+		document.getElementById("testDisplay").innerHTML = "Points: " + this.points;
+		document.getElementById("lastGuess").innerHTML = "Last Guess: ";
 		for(x = 0; x < this.answer.length; x++){
 			this.showLetter.pop(x);			
 		}
